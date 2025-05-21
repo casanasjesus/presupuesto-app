1470b4df0e39a8d93aab4b262abfcb948e0abae8
Jesús Casañas <jesusenrique9773@gmail.com>
2025-05-21 12:04:07 -0300

    envío datos form

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,17 +1,52 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IngresoService } from '../ingreso/ingreso.service';
+import { EgresoService } from '../egreso/egreso.service';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { Egreso } from '../egreso/egreso.model';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario.component.html',
 })
 export class FormularioComponent {
   tipo: string = 'ingresoOperacion';
+  descripcionInput: string | null = null;
+  valorInput: number | null = null;
+
+  constructor(
+    private readonly ingresoService: IngresoService,
+    private readonly egresoService: EgresoService
+  ) {}
 
   tipoOperacion(evento: Event) {
     const elementoSelect = evento?.target as HTMLSelectElement;
     this.tipo = elementoSelect.value;
+  }
+
+  agregarRegistro() {
+    if (this.descripcionInput !== null && this.valorInput !== null) {
+      if (this.tipo === 'ingresoOperacion') {
+        this.ingresoService.ingresos.push(
+          new Ingreso(this.descripcionInput, this.valorInput)
+        );
+      }
+
+      if (this.tipo === 'egresoOperacion') {
+        this.egresoService.egresos.push(
+          new Egreso(this.descripcionInput, this.valorInput)
+        );
+      }
+
+      this.descripcionInput = null;
+      this.valorInput = null;
+    }
+  }
+
+  datosIvalidos() {
+    alert('Debe introducir valores en descripción y valor');
   }
 }
